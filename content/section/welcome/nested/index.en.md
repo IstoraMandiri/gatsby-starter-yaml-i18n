6b7664f5b54e0207e2265dd94f7f10819cf6a29e
Chris Hitchcott <hitchcott@gmail.com>
2020-04-19 19:42:59 +0100

    Initial commit with barebones example

---
+++ content/section/welcome/nested/index.en.md
@@ -0,0 +1,11 @@
+---
+title: Welcome
+description: A welcome page
+author: Mr Jones
+date: 2020-01-10
+tags: ["something"]
+---
+
+# Welcome!
+
+This is an example page in english using just a markdown file.
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,21 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: { extensions: ['.mdx', '.md'] }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/content`,
+        name: 'yaml-i18n',
+      },
+    },
+    { 
+      resolve: 'gatsby-plugin-yaml-i18n',
+      options: {
+        locales: ['en', 'de'],
+      }
+    },
+  ],
 }
